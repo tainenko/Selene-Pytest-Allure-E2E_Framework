36d6877af8a5a60554c07a2dd12caf683abee411
柯泰年 <tony.ko@ehsn.com.tw>
2018-12-21 17:50:09 +0800

    correct searchresultpage

--- src/pages/searchresult_page.py
+++ src/pages/searchresult_page.py
@@ -2,6 +2,6 @@ from common import *
 from src.pages.page import BasePage
 from src.pages.main_page import MainPage
 
-class SearchResultPage()
+class SearchResultPage(BasePage)
     def __init__(self):
         self.items=ss("div.n-card__box")
